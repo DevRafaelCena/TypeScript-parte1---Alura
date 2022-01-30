1e76d69b3e5ca728a60ff9ca7cecd82ff9e562a9
Rafael Cena <rafael.cena@hotmail.com>
2022-01-30 13:20:38 -0300

    visibilidade e enums

--- app/views/mensagem-view.ts
+++ app/views/mensagem-view.ts
@@ -1,14 +1,11 @@
-import { View } from "./view";
 
-export class MensagemView extends View<String>{
+import { View } from './view.js';
 
+export class MensagemView extends View<string> {
 
-    template(model:string): string {
-        return  `
+    protected template(model: string): string {
+        return `
             <p class="alert alert-info">${model}</p>
-        `;
+        `
     }
-
- 
-
 }
